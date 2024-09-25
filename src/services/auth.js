d8f17a8b073edca68c746301dba77045effad906
Kwesi Navilot <andrewskwesiankomahene@gmail.com>
2024-09-25 16:02:00 +0000

    (feat|inst|enha) installed prisma && configured it to work with authjs && created first migration

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,15 +1,13 @@
 
 import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/prisma"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Google],
   pages: {
     signIn: "/auth",
-  },
-  callbacks: {
-    async redirect({ url, baseUrl }) {
-      return baseUrl
-    },
   },
 })
